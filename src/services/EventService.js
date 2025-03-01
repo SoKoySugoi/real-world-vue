4a639a9189cb2e3bd54235c4ced24d9435474a79
SoKoySugoi <daljay1006@gmail.com>
2025-02-28 20:56:00 -0800

    For sure this time

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/SoKoySugoi/real-world-vue',
+  baseURL: 'https://my-json-server.typicode.com/SoKoySugoi/my-repo',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
