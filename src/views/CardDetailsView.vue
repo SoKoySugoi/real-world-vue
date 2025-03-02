6995a9fd454c1e3a7f66db14ddd4f269e5ee2462
SoKoySugoi <daljay1006@gmail.com>
2025-03-01 19:18:19 -0800

    did stuff

--- src/views/CardDetailsView.vue
+++ src/views/CardDetailsView.vue
@@ -28,12 +28,30 @@ onMounted(() => {
         <div class="col-md-3"></div>
         <img class="col-md-3" v-bind:src="card.image" />
         <ul class="col-md-2 details">
-          <li v-if="card.attribute">Attribute: {{ card.attribute }}</li>
-          <li v-if="card.type">Type: {{ card.type }}</li>
-          <li v-if="card.attack || card.defense">ATK/DEF: {{ card.attack }}/{{ card.defense }}</li>
-          <li>Rarity: {{ card.rarity }}</li>
           <li>
-            Description:
+            <strong>Name: </strong> <span>{{ card.name }}</span>
+          </li>
+          <li v-if="card.attribute">
+            <strong>Attribute: </strong> <span>{{ card.attribute }}</span>
+          </li>
+          <li v-if="card.types">
+            <strong>Type: </strong>
+            <span v-for="(type, index) in card.types" :key="index">{{ type }}</span>
+          </li>
+          <li v-if="card.level">
+            <strong>Level/Link: </strong>
+            <span>{{ card.level }}</span>
+          </li>
+          <li v-if="card.attack || card.defense">
+            <strong>ATK/DEF: </strong>
+            <span>{{ card.attack }}/{{ card.defense }}</span>
+          </li>
+          <li>
+            <strong>Rarity: </strong>
+            <span>{{ card.rarity }}</span>
+          </li>
+          <li>
+            <strong>Description:</strong>
             <p>{{ card.description }}</p>
           </li>
         </ul>
