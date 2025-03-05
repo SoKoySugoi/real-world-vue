0e8830bc4dfea8560fcd053262c1e0c937d44080
SoKoySugoi <daljay1006@gmail.com>
2025-03-04 16:24:24 -0800

    offloaded some css

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -3,20 +3,20 @@
     <tbody v-if="cards.length >= 3">
       <tr class="row" v-for="(row, rowIndex) in tempArray" :key="rowIndex">
         <td class="col-md-4 mx-2" v-for="(card, i) in row" :key="i">
-          <card-component :card="card" />
+          <card-list-item :card="card" />
         </td>
       </tr>
     </tbody>
     <tbody v-else>
       <td v-for="(card, i) in cards" :key="i">
-        <card-component :card="card" />
+        <card-list-item :card="card" />
       </td>
     </tbody>
   </table>
 </template>
 
 <script setup>
-import CardComponent from '@/components/CardListItem.vue'
+import CardListItem from '@/components/CardListItem.vue'
 import { computed } from 'vue'
 const props = defineProps({
   cards: {
--- src/components/CardListItem.vue
+++ src/components/CardListItem.vue
@@ -1,23 +1,16 @@
 <template>
   <div>
-    <router-link
-      class="card-details-link"
-      :to="{ name: 'card-details', params: { id: props.card.id } }"
-    >
+    <router-link class="card-details-link" :to="{ name: 'card-details', params: { id: card.id } }">
       <strong>{{ card.name }}</strong>
       <div class="display-card">
-        <img v-bind:src="props.card.image" />
+        <img v-bind:src="card.image" />
       </div>
     </router-link>
   </div>
-  <!--<div v-else>
-    <h2>Your deck has no availible cards</h2>
-    <p>build your deck by selecting a card from the library.</p>
-  </div>-->
 </template>
 
 <script setup>
-const props = defineProps({
+defineProps({
   card: {
     type: Object,
   },
@@ -26,19 +19,12 @@ const props = defineProps({
 
 <style scoped>
 .display-card img {
-  padding: 20px;
   width: 150px;
   height: auto;
-  cursor: pointer;
-  border: 1px solid #aac9e7;
-  margin: 18px;
 }
 .display-card:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
-.card-details-link {
-  color: #aac9e7;
   text-decoration: none;
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+  transform: scale(1.1);
 }
 </style>
